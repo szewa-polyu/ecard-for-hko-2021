5ce9013a501bb9a8cbaf86c7ef532ea06e7cad9f
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2021-11-16 00:26:23 +0800

    fixed mobile issue by media query

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import Canvas from './Canvas';
-import useElementSize from 'hooks/useElementSize';
 import './App.css';
 
-const ecardContainerId = 'ecardContainer';
-
 const App = _ => {
-  const { width: ecardContainerWidth, height: ecardContainerHeight } =
-    useElementSize(ecardContainerId);
-
-  console.log(ecardContainerHeight);
-
   return (
     <div className='App'>
-      <div id={ecardContainerId} className='ecard-container'>
+      <div className='ecard-container'>
         <Canvas />
       </div>
     </div>
